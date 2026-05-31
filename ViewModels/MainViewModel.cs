81cafc5d2a619cfc22e0ed878c05c993e1b7bdcc
Андрей Бескровный <beskrovnyy_a@internet.ru>
2026-05-31 13:47:01 +0300

    -изменено сообщение журнала логов

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1012,7 +1012,7 @@ namespace NewAPP
         }
         public void ExecuteLoggerCommand(object param)
         {
-            MessageBox.Show("Логгер");
+            MessageBox.Show("Тут возможен журнал ошибок");
         }
         private void LoadAllNomenclature() //подгружает номенклатуру 
         {
